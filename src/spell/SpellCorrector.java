008f88984630c5adfdcad5b7f43498388832f87d
michaelblack117 <michaelblack117@gmail.com>
2019-01-17 11:19:07 -0700

    adding candidates for suggestSimilarWord function

--- src/spell/SpellCorrector.java
+++ src/spell/SpellCorrector.java
@@ -4,6 +4,7 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SpellCorrector implements ISpellCorrector {
@@ -17,7 +18,7 @@ public class SpellCorrector implements ISpellCorrector {
         String word;
 
         while (scanner.hasNext()) {
-            word = scanner.next();
+            word = scanner.next().toLowerCase();
             this.trieWords.add(word);
         }
         scanner.close();
@@ -25,6 +26,41 @@ public class SpellCorrector implements ISpellCorrector {
 
     public String suggestSimilarWord(String inputWord) {
         //FIXME
+        ArrayList candidates = new ArrayList();
+        ArrayList matches = new ArrayList();
+        int wordLength = inputWord.length();
+        char insertLetter = 'a';
+
+        if (trieWords.find(inputWord).isWord()) {
+            return inputWord;
+        }
+
+        // Add Deletion Candidates
+        for (int i = 0; i < wordLength; i++) {
+            candidates.add(inputWord.substring(0, i) + inputWord.substring(i + 1, wordLength));
+
+            // Add Alteration Candidates
+            for (int j = 0; j < 26; j++) {
+                insertLetter = (char) ('a' + j);
+                candidates.add(inputWord.substring(0, i) + insertLetter + inputWord.substring(i + 1, wordLength));
+            }
+        }
+
+        // Add Insertion Candidates
+        for (int i = 0; i <= wordLength; i++) {
+            for (int j = 0; j < 26; j++) {
+                insertLetter = (char) ('a' + j);
+                candidates.add(inputWord.substring(0, i) + insertLetter + inputWord.substring(i, wordLength));
+            }
+        }
+
+        // Add Transposition Candidates
+        for (int i = 0; i < wordLength - 1; i++) {
+            candidates.add(inputWord.substring(0, i) + inputWord.substring(i+1,i+2) + inputWord.substring(i,i+1)
+                    + inputWord.substring(i+2, wordLength));
+        }
+
+
         return "";
     }
 
